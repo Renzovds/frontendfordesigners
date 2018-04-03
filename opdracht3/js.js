7b6bbc3e70c252846c16379ed23eda50ba0bed91
Renzovds <31884302+Renzovds@users.noreply.github.com>
2018-04-03 10:42:09 +0200

    opdracht 3 met carousel

--- opdracht3/js.js
+++ opdracht3/js.js
@@ -1,7 +1,7 @@
 function getUserQuery() {
     var config = {
         html: '',
-        url: "https://blokweb.org/stories "
+        url: "http://dennistel.nl/movies "
     };
 
     var xhr = new XMLHttpRequest();
@@ -12,13 +12,41 @@ function getUserQuery() {
             var data = JSON.parse(xhr.responseText);
             console.log(data);
 
+            showMovies(data);
+
 
         } else {
             console.log("an error occured");
         }
     };
     xhr.send();
-
 }
-console.log("JOe");
-getUserQuery()
+
+getUserQuery();
+
+function showMovies(movies) {
+    var slider = document.querySelector('#movies');
+    for (var i = 0; i < movies.length; i++) {
+
+        var myArticle = document.createElement('article');
+        var myDiv = document.createElement('div');
+        var myH2 = document.createElement('h2');
+        var myP = document.createElement('p');
+
+
+
+        myP.textContent = movies[i].simple_plot;
+        myH2.textContent = movies[i].title;
+        myDiv.appendChild(myH2);
+        myDiv.appendChild(myP);
+        myDiv.classList.add('slide-content');
+        var bgImage = 'background-image: url(' + movies[i].cover + ')';
+        myArticle.setAttribute('style', bgImage);
+        myArticle.classList.add('slide');
+        myArticle.appendChild(myDiv);
+
+        slider.appendChild(myArticle);
+
+        createSlider();
+    }
+}
